fc451f82c3cd688b71f3baf4317529e82d547d8e
Ariane Kahl <ariane.kahl@stud.uni-goettingen.de>
2024-05-31 15:04:39 +0200

    clean up mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,57 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+import matplotlib.pyplot as plt
 
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+def merge_sort(to_sort):
+    if len(to_sort) > 1:
+        # part the list
+        mid = len(to_sort) // 2
+        left = to_sort[:mid]
+        right = to_sort[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        # recursive call of merge_sort
+        merge_sort(left)
+        merge_sort(right)
 
         l = 0
         r = 0
         i = 0
 
-        while l < len(left) and r < len(right):
+        # as long as both lists have elements
+        while l<len(left) and r<len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                to_sort[i] = left[l] 
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                to_sort[i]= right[r]
                 r += 1
             i += 1
-
+        # if only the left list has elements
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
+            to_sort[i] = left[l]
             l += 1
             i += 1
-
+        # if only the right list has elements
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+            to_sort[i] = right[r]
             r += 1
             i += 1
 
 
-import matplotlib.pyplot as plt
-
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
+plt.show() # plot of original list
+merge_sort(my_list)
 plt.plot(x, my_list)
-plt.show()
+plt.show() # plot of sorted list
+
+
+# changed ASSIGNMENT to assignment
+# gelöscht: and not len(to_sort) < 1
+# and len(to_sort) != 0
+# changed mergeSort to merge_sort
+# changed list_to_be_sorted_by_merch to to_sort    
+# def assignment(new_list, i, old_list, j): 
+#    new_list[i] = old_list[j]        rausgeschmissen
+# x = range(len(my_list)) rausgeschmissen
+# while l<len(left) and r<len(right): changed spacing
